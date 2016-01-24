a18aecdbe5216e942e0ccd5b71df08104ec05624
antoinetrouve <antoinetrouve.france@gmail.com>
2016-01-24 11:49:47 +0100

    Initial Commit

---
+++ CityTabProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityTabProject
+//
+//  Created by etudiant on 24/01/2016.
+//  Copyright © 2016 iia.antoinetrouve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
